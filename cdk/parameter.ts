4b5c2f205e4baf96c0f1588de5c9cff72b094afb
Arthur Milliken <amilliken@gmail.com>
2025-06-11 12:29:55 -0700

    6/11

--- cdk/parameter.ts
+++ cdk/parameter.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { BedrockChatParametersInput } from "./lib/utils/parameter-models";
 
 export const bedrockChatParams = new Map<string, BedrockChatParametersInput>();
@@ -5,4 +6,31 @@ export const bedrockChatParams = new Map<string, BedrockChatParametersInput>();
 // bedrockChatParams.set("dev", {});
 
 // If you define "default" environment here, parameters in cdk.json are ignored
-// bedrockChatParams.set("default", {});
+bedrockChatParams.set("default", {
+  bedrockRegion: "us-east-1",
+  allowedIpV4AddressRanges: ["0.0.0.0/1", "128.0.0.0/1"],
+  allowedIpV6AddressRanges: [
+    "0000:0000:0000:0000:0000:0000:0000:0000/1",
+    "8000:0000:0000:0000:0000:0000:0000:0000/1",
+  ],
+  identityProviders: [],
+  userPoolDomainPrefix: "",
+  allowedSignUpEmailDomains: [],
+  autoJoinUserGroups: ["CreatingBotAllowed"],
+  selfSignUpEnabled: false,
+  publishedApiAllowedIpV4AddressRanges: ["0.0.0.0/1", "128.0.0.0/1"],
+  publishedApiAllowedIpV6AddressRanges: [
+    "0000:0000:0000:0000:0000:0000:0000:0000/1",
+    "8000:0000:0000:0000:0000:0000:0000:0000/1",
+  ],
+  enableRagReplicas: false,
+  enableBedrockCrossRegionInference: true,
+  enableLambdaSnapStart: true,
+  enableBotStore: false,
+  enableBotStoreReplicas: false,
+  botStoreLanguage: "en",
+  tokenValidMinutes: 30,
+  alternateDomainName: "",
+  hostedZoneId: "",
+  devAccessIamRoleArn: "",
+});
